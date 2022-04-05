24f04c679fc4d5d3f71a5b0bf94c700c1c8b2ea6
ahmadbariqs <101858455+ahmadbariqs@users.noreply.github.com>
2022-04-05 14:36:20 +0700

    Add files via upload

---
+++ ForLoopClass1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AHMAD BARIQ
+ */
+public class ForLoopClass1 {
+  public static void main(String[] args) {
+
+    int n = 7;
+    
+    for (int i = 1; i < n; ++i) {
+      System.out.println("I Love Java !!!");
+    }
+  }
+} 
+
