a3c564cae1eea98164fa64b754d1b68f0d25fa6d
ahmadbariqs <101858455+ahmadbariqs@users.noreply.github.com>
2022-04-05 16:17:12 +0700

    Add files via upload

---
+++ IfConditionalClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AHMAD BARIQ
+ */
+public class IfConditionalClass {
+    public static void main(String[] args) {
+        int age = 18;
+        
+        if (age <= 5){
+        System.out.println("Bayi"); 
+        }
+        else if (age <= 12){
+        System.out.println("Anak-anak");  
+        }
+        else if (age <= 21){
+        System.out.println("Remaja");  
+        }
+        else {
+          System.out.println("Orang tua");   
+        }
+    }
+}
